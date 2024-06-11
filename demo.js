39e55cc866ce977a72411f5f3cf97dbcf3145a7b
Jose Ricardo Casallas <josericardocasasllas11@gmail.com>
2024-06-11 00:18:27 -0500

    ejercicio demo

---
+++ demo.js
@@ -0,0 +1,11 @@
+function miFuncion(nombre, apellido, edad){
+  
+    console.log("el nombre de la persona es: ", nombre)
+    console.log("el nombre de la persona es: ", apellido)
+    console.log("el apellido de la persona es: ", edad)
+    
+    
+    }
+    miFuncion("william","morales", 22)
+    miFuncion("rut","sepulveda", 22)
+    miFuncion("leonardo","gonzales", 22)
